90631cef12e9e09b9bd429fa4964f2e1dfc4e31e
Vannear <30491653+vannearpor@users.noreply.github.com>
2018-06-03 16:14:21 -0700

    database population

--- server/api/restaurants.js
+++ server/api/restaurants.js
@@ -5,7 +5,7 @@ import db from '../db'
 const router = Router()
 
 router.get('/restaurants', function (req, res, next) {
-  db.all('SELECT id, name, lat, lng FROM test_restaurant', function (err, rows) {
+  db.all('SELECT restaurant_id, name, lat, lng FROM restaurant', function (err, rows) {
     if (err) {
       next(err)
     }
@@ -15,7 +15,7 @@ router.get('/restaurants', function (req, res, next) {
 
 router.get('/restaurants/:id', function (req, res, next) {
   db.get(
-    'SELECT id, name, lat, lng FROM test_restaurant WHERE id = (?)',
+    'SELECT restaurant_id, name, lat, lng FROM restaurant WHERE id = (?)',
     [req.params.id],
     function (err, row) {
       res.json(row)
