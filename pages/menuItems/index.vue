cd7912b0b18e6d026d7b7c3e5387cbeb061acfb2
Zach Morrissey <zachmor05@gmail.com>
2018-06-03 20:51:55 -0700

    minimum viable menuItems route

---
+++ pages/menuItems/index.vue
@@ -0,0 +1,75 @@
+<template>
+  <section class="container">
+    <h1 class="title">Menu Items</h1>
+    <table class="menu-table">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Calories</th>
+          <th>Carbohydrates</th>
+          <th>Sodium</th>
+          <th>Protein</th>
+          <th>Fat</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item, index) in menuItems" :key="index" class="menu-item">
+          <td>
+            <nuxt-link :to="'/menuItems/' + item.menu_id">
+              {{ item.name }}
+            </nuxt-link>
+          </td>
+          <td>{{ item.cal }}</td>
+          <td>{{ item.carb }}</td>
+          <td>{{ item.sodium }}</td>
+          <td>{{ item.protein }}</td>
+          <td>{{ item.fat }}</td>
+        </tr>
+      </tbody>
+    </table>
+
+  </section>
+</template>
+
+<script>
+import axios from '~/plugins/axios'
+
+export default {
+  async asyncData () {
+    let { data } = await axios.get('/api/menuItems')
+    return { menuItems: data }
+  },
+  head () {
+    return {
+      title: 'Menu Items'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.title
+{
+  color: #e53737;
+  margin: 30px 0;
+}
+.menu-table
+{
+  margin-left: auto;
+  margin-right: auto;
+}
+.menu-table th,
+.menu-table td {
+  padding: 0.25rem;
+}
+.menu-items
+{
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+.menu-item
+{
+  margin: 10px 0;
+}
+</style>
--- server/api/index.js
+++ server/api/index.js
@@ -2,11 +2,13 @@ import { Router } from 'express'
 
 import users from './users'
 import restaurants from './restaurants'
+import menuItems from './menuItems'
 
 const router = Router()
 
 // Add USERS Routes
 router.use(users)
 router.use(restaurants)
+router.use(menuItems)
 
 export default router
