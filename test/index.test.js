21717b7ad7dbde7c50fd1275fb72814e3dca0ca0
Zach Morrissey <zachmor05@gmail.com>
2018-06-06 20:08:05 -0700

    add tests for footer and header

--- test/index.test.js
+++ test/index.test.js
@@ -17,17 +17,26 @@ test.before(async () => {
   await nuxt.listen(3000, 'localhost')
 }, 30000)
 
-// Example of testing only generated html
+// basic tests using only main page
 test('Web Server is working', async t => {
   const context = {}
   const { html } = await nuxt.renderRoute('/', context)
   t.true(html.includes('<!DOCTYPE html>'))
 })
 
-// Example of testing via DOM checking
-// test('Route / exits and render HTML with CSS applied', async t => {
-//   const window = await nuxt.renderAndGetWindow('http://localhost:3000/')
-// })
+test('Header shows up', async t => {
+  const context = {}
+  const { html } = await nuxt.renderRoute('/', context)
+  t.true(html.includes('<header>'))
+})
+
+test('Footer shows up', async t => {
+  const context = {}
+  const { html } = await nuxt.renderRoute('/', context)
+  t.true(html.includes('<footer>'))
+})
+
+
 
 // Close server and ask nuxt to stop listening to file changes
 test.after('Closing server and nuxt.js', t => {
