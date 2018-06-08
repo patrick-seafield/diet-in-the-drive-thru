dd834e449b3261a39c0f08d7884f6c95c78731f7
Zach Morrissey <morrissz@oregonstate.edu>
2018-06-08 08:14:08 -0700

    Adding photo credit in footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,16 @@
 <template>
   <footer>
-    <strong>Diet in the Drive-Thru</strong>
+    <p><strong>Diet in the Drive-Thru</strong></p>
+    <p class="tagline">Copyright 2018 - OSU CS361 Group 4</p>
+    <p class="tagline">Frontpage photo courtesty of <a href="https://www.pexels.com/photo/food-dinner-lunch-unhealthy-70497/">Robin Stickel</a></p>
   </footer>
 </template>
+
+<style scoped>
+
+.tagline
+{
+  font-size: 0.75rem;
+}
+
+</style>
